8298e120083b439be2a6a5eb4f7ed879cfff575d
Kojiro.tanaka <kojiro.tanaka@ten-mado.net>
2020-03-18 17:06:14 +0900

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
 
 aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
 kubectl create secret docker-registry "$INPUT_SECRET_NAME" \
-  --docker-server="$INPUT_DCOKER_SERVER" \
+  --docker-server="$INPUT_DOCKER_SERVER" \
   --docker-username="$INPUT_DOCKER_USERNAME" \
   --docker-password="$INPUT_DOCKER_PASSWORD" \
   --docker-email="$INPUT_DOCKER_EMAIL"
